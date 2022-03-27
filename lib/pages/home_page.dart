21843942bb87c77b35706ee9d1dd0c60de4ba153
Zaed Abdullah <z4ed.thalib123@gmail.com>
2022-03-27 16:30:51 +0700

    Wrap card with InkWell widget

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import '../models/item.dart';
+
+class HomePage extends StatelessWidget{
+  HomePage({Key? key}) : super(key: key);
+  final List<Item> items = [
+    Item(name: 'Sugar', price: 5000),
+    Item(name: 'Salt', price: 2000),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("List Belanjaan"),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(8),
+        child: ListView.builder(
+          itemBuilder: (context, index){
+            final item = items[index];
+            return InkWell(
+              onTap: () {
+                Navigator.pushNamed(context, '/item');
+              },
+              child: Card(
+                child: Container(
+                  margin: EdgeInsets.all(8),
+                  child: Row(
+                    children: [
+                      Expanded(child: Text(item.name)),
+                      Expanded(
+                        child: Text(
+                          item.price.toString(),
+                          textAlign: TextAlign.end,
+                        )
+                      )
+                    ]
+                  ),
+                ),
+              ),
+            );
+          }
+        ),
+      )
+    );
+  }
+}
